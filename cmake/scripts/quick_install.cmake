0ad7073cc14cb549ac6b68a726498864db3f2f99
Aymeric Pellé <aympelle@gmail.com>
2023-07-10 00:31:22 +0200

    v0.2.0:#7: Update quick install script and move it to cmake/script/.

--- cmake/scripts/quick_install.cmake
+++ cmake/scripts/quick_install.cmake
@@ -1,4 +1,6 @@
-# cmake -P cmake_quick_install.cmake
+# cmake -P quick_install.cmake
+
+set(project "arba-wgen")
 
 function(get_script_args script_args)
     set(sc_args)
@@ -16,12 +18,10 @@ function(get_script_args script_args)
 endfunction()
 
 get_script_args(args)
-set(options "")
+set(options "TESTS")
 set(params DIR BUILD)
 set(lists "")
 cmake_parse_arguments(ARG "${options}" "${params}" "${lists}" ${args})
-
-set(project "wgen")
 
 if(WIN32)
     set(temp_dir $ENV{TEMP})
@@ -31,10 +31,9 @@ else()
     message(FATAL_ERROR "No temporary directory found!")
 endif()
 
-file(TO_NATIVE_PATH "/" path_sep)
-set(src_dir ${CMAKE_CURRENT_LIST_DIR})
-set(build_dir ${temp_dir}${path_sep}${project}-build)
-set(error_file ${build_dir}${path_sep}quick_install_error)
+set(src_dir ${CMAKE_CURRENT_LIST_DIR}/../..)
+set(build_dir ${temp_dir}/local/build/${project}-build)
+set(error_file ${build_dir}/quick_install_error)
 
 if(EXISTS ${error_file})
     message(STATUS "Previous call to quick_install.cmake failed. Cleaning...")
@@ -42,14 +41,21 @@ if(EXISTS ${error_file})
 endif()
 
 message(STATUS "*  CONFIGURATION")
+# arg TESTS:
+if(ARG_TESTS)
+    list(APPEND conf_args -DBUILD_${project}_TESTS=ON)
+endif()
+# arg BUILD:
 if(ARG_BUILD)
     list(APPEND conf_args -DCMAKE_BUILD_TYPE=${ARG_BUILD})
-else()
-    list(APPEND conf_args -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE})
 endif()
+# arg DIR:
 if(ARG_DIR)
     list(APPEND conf_args -DCMAKE_INSTALL_PREFIX=${ARG_DIR})
+elseif(NOT "$ENV{MINGW_PREFIX}" STREQUAL "")
+    list(APPEND conf_args -DCMAKE_INSTALL_PREFIX=$ENV{MINGW_PREFIX})
 endif()
+# execute CMake:
 execute_process(COMMAND ${CMAKE_COMMAND} ${conf_args} -S ${src_dir} -B ${build_dir}  RESULT_VARIABLE cmd_res)
 if(NOT cmd_res EQUAL 0)
     file(TOUCH ${error_file})
@@ -61,6 +67,15 @@ execute_process(COMMAND ${CMAKE_COMMAND} --build ${build_dir}  RESULT_VARIABLE c
 if(NOT cmd_res EQUAL 0)
     file(TOUCH ${error_file})
     return()
+endif()
+
+if(ARG_TESTS)
+  message(STATUS "*  TESTS")
+  execute_process(COMMAND ${CMAKE_CTEST_COMMAND}  WORKING_DIRECTORY ${build_dir} RESULT_VARIABLE cmd_res)
+  if(NOT cmd_res EQUAL 0)
+      file(TOUCH ${error_file})
+      return()
+  endif()
 endif()
 
 message(STATUS "*  INSTALL")
